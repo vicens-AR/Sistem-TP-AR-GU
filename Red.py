8edfd5a400c2ecc988730a39d9a51f1a8b405536
Vicente Arhex Harwigt <d47797531@instituto.itr.edu.ar>
2024-04-15 16:40:43 -0300

    commit del mati y el vicens

--- Red.py
+++ Red.py
@@ -1,9 +1,43 @@
 class Nodo:
-    def __init__(self,serv,C1,C2,C3):
-        self.servidor = serv
-        self.cliente1 = C1
-        self.cliente2 = C2
-        self.cliente3 = C3
+    def __init__(self, nombre):
+       self.nombre = nombre
+       self.conexiones = []
+      
 
-    def Agregar_conexion(self):
-        print(f"{self.servidor} se conecto con{self.cliente1}")
+    def Agregar_conexion(self,nodo):
+        self.conexiones.append(nodo)
+        
+    def enviar_mensaje(self,mensaje):
+        print(f"{self.nombre} has enviado un mensaje que dice: {mensaje}")
+        for conexion in self.conexiones:
+            conexion.recibir_mensaje(mensaje)
+
+         
+    def recibir_mensaje(self, mensaje):
+        print(f"{self.nombre} recibio un mensaje: {mensaje}") 
+
+
+#crear nodos
+servidor = Nodo("Servidor")
+
+cliente1 = Nodo("Martina")
+cliente2 = Nodo("Negro")
+cliente3 = Nodo("Carla")
+
+
+#establer conexiones
+cliente1.Agregar_conexion(servidor)
+cliente2.Agregar_conexion(servidor)
+cliente3.Agregar_conexion(servidor)
+servidor.Agregar_conexion(cliente3)
+servidor.Agregar_conexion(cliente2)
+servidor.Agregar_conexion(cliente1)
+
+
+#mensajes enviados
+servidor.enviar_mensaje(input("ingresa tu mensaje:"))
+cliente1.recibir_mensaje()
+cliente2.recibir_mensaje()
+cliente3.recibir_mensaje()
+
+
